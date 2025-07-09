33b3ed8721a57a3059917078c1d51d33de2b1b1c
rajashre-e <145351880+rajashre-e@users.noreply.github.com>
2025-07-09 23:57:55 +0530

    My Sentences

--- routes/flashcard.js
+++ routes/flashcard.js
@@ -5,6 +5,10 @@ const {
 
   deleteFlashcard,
   updateFlashcard,
+  generateSentence,
+  saveSentence,
+  viewSentence,
+  deleteSentence,
 } = require("../controllers/flashcard");
 
 const router = Router();
@@ -31,5 +35,13 @@ router.post("/create", requireAuth, createflashcard);
 router.delete("/delete/:id", requireAuth, deleteFlashcard);
 
 router.put("/:id/update", requireAuth, updateFlashcard);
+
+router.get("/generate/:id", requireAuth, generateSentence);
+
+router.post("/:id/save-sentence", requireAuth, saveSentence);
+
+router.get("/view-sentences", requireAuth, viewSentence);
+
+router.delete("/sentence/delete/:id", requireAuth, deleteSentence);
 
 module.exports = router;
